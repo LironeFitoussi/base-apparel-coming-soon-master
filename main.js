3243f621a7ed1b876277743764cb6a9dcd2722e2
LironeFitoussi <lironefit@icloud.com>
2023-10-05 19:19:18 +0300

    error animation set

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 console.log("test");
-document.querySelector("#")
+
+$("form").submit(function (e) { 
+    e.preventDefault();
+    const userMail = $("#mailInput").val()
+
+    // Ends with validation
+    const hasDotCom = userMail.endsWith(".com");
+    const hasDotOrg = userMail.endsWith(".org");
+    const hasDotNet = userMail.endsWith(".net");
+    const hasDotEdu = userMail.endsWith(".edu");
+    if (hasDotCom || hasDotOrg || hasDotNet || hasDotEdu) {
+        console.log("hasDotCom");
+    } else {
+        return printWrongAdrress()
+    }
+});
+
+function printWrongAdrress() {
+    $(".error-sign").css("display","block")
+    $(".error-sign").toggleClass("animated");
+    setTimeout(() => {
+        $(".error-sign").toggleClass("animated");
+    }, 500);
+    console.log("Wrong Address");
+}
